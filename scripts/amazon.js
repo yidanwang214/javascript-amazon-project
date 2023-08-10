ab7888294ce932baf83d7de31ba83224d149826d
yidanwang214 <yidan.eden.wang@gmail.com>
2023-08-10 23:53:10 +0930

    Move addToCart into cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 
 let productHTML = '';
@@ -65,33 +65,21 @@ products.forEach((product) => {
 document.querySelector('.js-products-grid').innerHTML
 = productHTML;
 
+function updateCartQuantity(){
+  let cartQuantity = 0;
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  document.querySelector('.cart-quantity').innerHTML
+  = `${cartQuantity}`;
+}
+
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
     // console.log(button.dataset.productName); // eg: Intermediate Size Basketball
     const productId = button.dataset.productId;
-    
-    let matchingItem;
-
-    cart.forEach((item) => {
-      if(productId === item.productId){
-         matchingItem = item;
-      }
-    })
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: 1
-      });
-    }
-
-    let cartQuantity = 0;
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
-    });
-
-    document.querySelector('.cart-quantity').innerHTML
-    = `${cartQuantity}`;
+    addToCart(productId);
+   updateCartQuantity();
   });
 })
