66c30aa6e6c10a5f485dc0fdd080cb459028d3e0
yidanwang214 <yidan.eden.wang@gmail.com>
2023-08-10 23:21:53 +0930

    import products from products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,6 @@
-import { cart } from "../data/cart";
+import { cart } from "../data/cart.js";
+import { products } from "../data/products.js";
+
 let productHTML = '';
 
 // Syntax for a Data Attribute
